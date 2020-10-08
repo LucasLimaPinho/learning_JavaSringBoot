de9574ace96db8e03513f06e28a058833b1eb3b7
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-08 19:53:30 -0300

    Java API with Spring Boot

---
+++ src/test/java/com/claro/springboot/URA4/test/JavaAPISpringBoot/JavaApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claro.springboot.URA4.test.JavaAPISpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
